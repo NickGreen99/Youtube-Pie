febdc7568613b7fcdd0a67d6d3a6267999f75069
Sotiris Geo <sotirisgeo99@gmail.com>
2022-05-08 14:16:43 +0300

    getting subscriptions with oauth2

--- video1.py
+++ video1.py
@@ -1,18 +1,63 @@
-from urllib import response
-from googleapiclient.discovery import build
 
 import os
+import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
 
-api_key = os.environ.get("GoogleAPIKey")
+credentials = None
 
-youtube = build("youtube","v3", developerKey= api_key)
+# token.pickle stores the user's credentials from previously successful logins
+if os.path.exists('token.pickle'):
+    print('Loading Credentials From File...')
+    with open('token.pickle', 'rb') as token:
+        credentials = pickle.load(token)
 
-request = youtube.channels().list(
-        part = "statistics",
-        id = 'UC5ZZcLjzSCPga1LRYXylBhA' #different from username that used in tutorial its mine
+
+# If there are no valid credentials available, then either refresh the token or log in.
+if not credentials or not credentials.valid:
+    if credentials and credentials.expired and credentials.refresh_token:
+        print('Refreshing Access Token...')
+        credentials.refresh(Request())
+    else:
+        print('Fetching New Tokens...')
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'client_secrets.json',
+            scopes=[
+                'https://www.googleapis.com/auth/youtube.readonly'
+            ]
+        )
+
+        flow.run_local_server(port=8080, prompt='consent',
+                              authorization_prompt_message='')
+        credentials = flow.credentials
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as f:
+            print('Saving Credentials for Future Use...')
+            pickle.dump(credentials, f)
+
+
+
+
+
+
+
+youtube = build("youtube","v3", 
+    credentials=credentials
+    #developerKey= api_key
+)
+
+request = youtube.subscriptions().list(
+        part = "snippet,contentDetails",
+        channelId = "UC5ZZcLjzSCPga1LRYXylBhA",
+        prettyPrint = True,
+        alt = "json"
 
     )
 
 response = request.execute()
 
-print(response)
+for item in response['items']:
+    print(item['snippet'])
+    print()
