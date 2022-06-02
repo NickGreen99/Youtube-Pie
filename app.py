4ecab2e900f69cab1482544e41495204466f4617
NickGreen99 <nprasinos99@gmail.com>
2022-06-02 15:57:59 +0300

    Extended Flask support though there is a problem when accessing user stats

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from googleapiclient.discovery import build
 import liked
 import subscriptions
 import authentication
 
-
-liked_playlist = True
-subscribed = False
-cred = authentication.oauth()
-youtube = build("youtube", 'v3', credentials=cred)
-if liked_playlist:
-    liked_pl = liked.liked_playlist(youtube)
-if subscribed:
-    sub = subscriptions.subscribed_channels(youtube)
-
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return str(liked_pl)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get('log') == "Login using Google account":
+            cred = authentication.oauth()
+            yt = build("youtube", 'v3', credentials=cred)
+            return redirect(url_for('index', youtube=yt))
+    return render_template("login.html")
+
+
+@app.route('/index/<youtube>', methods=['POST', 'GET'])
+def index(youtube):
+    print(request.method)
+    if request.method == 'POST':
+        print("wow")
+        if request.form.get('sub') == 'Subscriptions':
+            print(youtube)
+            sub = subscriptions.subscribed_channels(youtube)
+
+            return str(sub)  # do something
+        elif request.form.get('liked') == 'Liked Playlists':
+            liked_pl = liked.liked_playlist(youtube)
+            return str(liked_pl)  # do something else
+    elif request.method == 'GET':
+        print("a")
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
