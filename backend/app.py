1f7ae3c5ce81e0067540f3de20740f708b0e4850
NickGreen99 <nprasinos99@gmail.com>
2022-06-17 15:50:07 +0300

    Test

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for
 from googleapiclient.discovery import build
-from backend import liked,subscriptions,authentication
+from backend import liked, subscriptions, authentication
+import pickle
 
 app = Flask(__name__, template_folder='../templates')
+token_file = "../token.pickle"
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        if request.form.get('log') == "Login using Google account":
-            return redirect(url_for('index'))
-    else:
-        return render_template("login.html")
+        if request.form.get('log') == "login":
+            authentication.oauth()
+            return redirect(url_for("index"))
+    return render_template("login.html")
 
 
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-    cred = authentication.oauth()
     if request.method == 'POST':
+        with open(token_file, "rb") as token:
+            cred = pickle.load(token)
         youtube = build("youtube", 'v3', credentials=cred)
-        if request.form.get('sub') == 'Subscriptions':
+        if request.form.get('sub') == 'subscriptions':
             sub = subscriptions.subscribed_channels(youtube)
             return render_template("index.html", pref=str(sub))
-        if request.form.get('liked') == 'Liked Playlists':
+        if request.form.get('liked') == 'liked_pl':
             liked_pl = liked.liked_playlist(youtube)
             return render_template("index.html", pref=str(liked_pl))
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
 
+print("Hello World")
 
 if __name__ == "__main__":
     app.run(debug=True)
