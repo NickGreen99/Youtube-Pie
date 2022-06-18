9eef33c1b22d0f02ddad4a8f20ef63665d2689a1
Sotiris Geo <sotirisgeo99@gmail.com>
2022-06-18 19:24:30 +0300

    fixed fonts and google button

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from googleapiclient.discovery import build
-from backend import liked, subscriptions, authentication
+import liked, subscriptions, authentication
 import pickle
 
 
